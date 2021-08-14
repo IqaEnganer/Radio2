e8e9bfed5eb22a0b7e04b306e7f6483e5e20e4d4
iqaenganer <daw.camalian@yandex.ru>
2021-08-14 19:03:58 +0300

    addcom

--- src/test/java/ru/netology/radioTest.java
+++ src/test/java/ru/netology/radioTest.java
@@ -3,7 +3,7 @@ package ru.netology;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+//Comments
 class radioTest {
     // Test stations
     // Switching by buttons
