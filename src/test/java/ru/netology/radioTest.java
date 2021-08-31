500699168ff60274dcbc41af719e42c9ec2f306b
iqaenganer <daw.camalian@yandex.ru>
2021-08-31 15:57:46 +0300

    FixTest

--- src/test/java/ru/netology/radioTest.java
+++ src/test/java/ru/netology/radioTest.java
@@ -10,6 +10,8 @@ class RadioTest {
     @Test
     public void showStatistic() {
         Radio rad = new Radio();
+        Radio rad2 = new Radio(true , "RadioRec", 25, 10, 20, 4,
+                0 , 100 , 10);
         assertEquals(false, rad.isOn());
         assertEquals("NotName", rad.getRadioName());
         rad.setCurrentStation(7);
@@ -19,7 +21,14 @@ class RadioTest {
         rad.setCurrentStation(9+1);
         assertEquals(10,rad.getCurrentStation());
         assertEquals(100, rad.getMaxVolume());
+        assertEquals(100, rad2.getMaxVolume());
+        assertEquals(true, rad2.isOn());
+        assertEquals("RadioRec", rad2.getRadioName());
+        assertEquals(25, rad2.getId());
+        assertEquals(10, rad2.getMinStation());
+
     }
+
 
 }
 
